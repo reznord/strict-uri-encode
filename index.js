5a0df2dc17cb8c1e91fde922998dff977ad26100
Anup <allamsetty.anup@gmail.com>
2019-12-31 09:17:29 +0530

    Remove arrow function

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+/* eslint-disable prefer-arrow-callback */
 'use strict';
-module.exports = string => encodeURIComponent(string).replace(/[!'()*]/g, x => `%${x.charCodeAt(0).toString(16).toUpperCase()}`);
+
+module.exports = function (string) {
+	return encodeURIComponent(string).replace(/[!'()*]/g, function (x) {
+		return '%'.concat(x.charCodeAt(0).toString(16).toUpperCase());
+	});
+};
